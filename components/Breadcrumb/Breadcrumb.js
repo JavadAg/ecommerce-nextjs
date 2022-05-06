8c167455b1c13979c74705600a21cbf8b8139ab5
JavadAg <j.aghebati93@gmail.com>
2022-05-06 23:59:08 +0430

    update Breadcrumb.js

--- components/Breadcrumb/Breadcrumb.js
+++ components/Breadcrumb/Breadcrumb.js
@@ -7,17 +7,17 @@ const Breadcrumb = () => {
       <div className="mt-24 w-full flex justify-center items-center flex-col bg-white ">
         <div className="p-10">
           <nav
-            class="flex py-3 px-5 text-gray-700 bg-gray-50 rounded-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700"
+            className="flex py-3 px-5 text-gray-700 bg-gray-50 rounded-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700"
             aria-label="Breadcrumb"
           >
-            <ol class="inline-flex items-center space-x-1 md:space-x-3">
-              <li class="inline-flex items-center">
+            <ol className="inline-flex items-center space-x-1 md:space-x-3">
+              <li className="inline-flex items-center">
                 <a
                   href="#"
-                  class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
+                  className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
                 >
                   <svg
-                    class="mr-2 w-4 h-4"
+                    className="mr-2 w-4 h-4"
                     fill="currentColor"
                     viewBox="0 0 20 20"
                     xmlns="http://www.w3.org/2000/svg"
@@ -28,42 +28,42 @@ const Breadcrumb = () => {
                 </a>
               </li>
               <li>
-                <div class="flex items-center">
+                <div className="flex items-center">
                   <svg
-                    class="w-6 h-6 text-gray-400"
+                    className="w-6 h-6 text-gray-400"
                     fill="currentColor"
                     viewBox="0 0 20 20"
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
-                      fill-rule="evenodd"
+                      fillRule="evenodd"
                       d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                      clip-rule="evenodd"
+                      clipRule="evenodd"
                     ></path>
                   </svg>
                   <a
                     href="#"
-                    class="ml-1 text-sm font-medium text-gray-700 hover:text-gray-900 md:ml-2 dark:text-gray-400 dark:hover:text-white"
+                    className="ml-1 text-sm font-medium text-gray-700 hover:text-gray-900 md:ml-2 dark:text-gray-400 dark:hover:text-white"
                   >
                     Templates
                   </a>
                 </div>
               </li>
               <li aria-current="page">
-                <div class="flex items-center">
+                <div className="flex items-center">
                   <svg
-                    class="w-6 h-6 text-gray-400"
+                    className="w-6 h-6 text-gray-400"
                     fill="currentColor"
                     viewBox="0 0 20 20"
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
-                      fill-rule="evenodd"
+                      fillRule="evenodd"
                       d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                      clip-rule="evenodd"
+                      clipRule="evenodd"
                     ></path>
                   </svg>
-                  <span class="ml-1 text-sm font-medium text-gray-400 md:ml-2 dark:text-gray-500">
+                  <span className="ml-1 text-sm font-medium text-gray-400 md:ml-2 dark:text-gray-500">
                     Flowbite
                   </span>
                 </div>
