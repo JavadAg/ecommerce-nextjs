2e648b8c274c5bf1d7be20a8ab3f31f2f26f3c90
JavadAg <j.aghebati93@gmail.com>
2022-05-07 00:22:52 +0430

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    images: { layoutRaw: true }
-  },
   images: { domains: ["img.stadiumgoods.com", "localhost"] },
   reactStrictMode: true
 }
