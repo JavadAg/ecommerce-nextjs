0ac985939f76bfb23be1b572b4f06aa3810a70e6
Javad aghebati <j.aghebati93@gmail.com>
2022-05-17 23:53:55 +0430

    added support for individual mobile banner

--- components/Hero/EmblaCarousel.js
+++ components/Hero/EmblaCarousel.js
@@ -1,17 +1,20 @@
 import React, { useState, useEffect, useCallback } from "react"
 import { DotButton, PrevButton, NextButton } from "./EmblaButtons"
 import useEmblaCarousel from "embla-carousel-react"
-/* import { mediaByIndex } from "../../../constants/index" */
+
 import Image from "next/image"
+import { useRouter } from "next/router"
 
 const slides = [
   {
-    img: "/banner1.png"
+    img: "/banner1.png",
+    imgMobile: "/banner1_mobile.png"
   },
-  { img: "/banner2.png" }
+  { img: "/banner2.png", imgMobile: "/banner1_mobile.png" }
 ]
 
 const EmblaCarousel = () => {
+  const router = useRouter()
   const [viewportRef, embla] = useEmblaCarousel({ skipSnaps: false })
   const [prevBtnEnabled, setPrevBtnEnabled] = useState(false)
   const [nextBtnEnabled, setNextBtnEnabled] = useState(false)
@@ -40,7 +43,7 @@ const EmblaCarousel = () => {
   }, [embla, setScrollSnaps, onSelect])
 
   return (
-    <>
+    <div>
       <div className="relative rounded-lg p-1 flex justify-center items-center">
         <div
           className="overflow-hidden w-full max-w-[2000px] "
@@ -48,24 +51,26 @@ const EmblaCarousel = () => {
         >
           <div className="flex">
             {slides.map((item, index) => (
-              <div className="relative  min-w-full " key={index}>
-                <div className="relative min-h-64 overflow-hidden">
+              <div className="relative min-w-full" key={index}>
+                <div className="relative overflow-hidden">
                   <Image
-                    className="object-cover min-h-full min-w-full rounded-2xl"
-                    src={item.img}
-                    width="2000px"
-                    height="680px"
+                    className="object-cover min-h-full max-h-[900px] min-w-full rounded-2xl "
+                    src={item.imgMobile}
+                    width="2000"
+                    height="900"
                     layout="responsive"
                     alt="banner"
                   />
-                  <div className="absolute bg-slate-50/30 m-0 top-2/4 left-4 rounded-2xl px-8 py-4 h-2/4 z-10 -translate-y-2/4 flex justify-around flex-col items-center backdrop-blur-2xl">
-                    <p className="text-6xl font-black text-slate-700 ">
-                      The Fashion of Champion
-                    </p>
-                    <button className="bg-red-400 text-white px-4 rounded-xl font-bold text-sm py-2">
-                      Shop Now
-                    </button>
-                  </div>
+                </div>
+                <div className="relative overflow-hidden hidden">
+                  <Image
+                    className="object-cover min-h-full max-h-[900px] min-w-full rounded-2xl "
+                    src={item.img}
+                    width="2000"
+                    height="900"
+                    layout="responsive"
+                    alt="banner"
+                  />
                 </div>
               </div>
             ))}
@@ -85,7 +90,24 @@ const EmblaCarousel = () => {
           ))}
         </div>
       </div>
-    </>
+      <div className="relative -mt-4 px-6 w-full">
+        <div className="bg-white border shadow-sm space-y-2 py-2 px-2 rounded-2xl flex justify-around flex-col items-center ">
+          <span className="text-xl font-black text-red-400 ">
+            The Fashion of Champion
+          </span>
+          <p className="text-center text-sm text-gray-900/70">
+            Shop official merch from HappyFeet, the world’s premier sneaker and
+            streetwear marketplace.
+          </p>
+          <button
+            onClick={() => router.push("/shop")}
+            className="bg-red-400/80 text-white px-2 rounded-xl font-bold text-sm py-1 italic"
+          >
+            Buy Now
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
