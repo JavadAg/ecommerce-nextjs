41de87a2e9045aadc36c9f1375976b0b36b2ec09
Javad aghebati <j.aghebati93@gmail.com>
2022-05-17 23:52:29 +0430

    update MobileNavbar.js

--- components/Header/Mobile/MobileNavbar.js
+++ components/Header/Mobile/MobileNavbar.js
@@ -9,6 +9,7 @@ import {
 } from "react-icons/hi"
 import Link from "next/link"
 import { useSelector } from "react-redux"
+import Search from "../../Search/Search"
 
 const navItems = [
   {
@@ -27,11 +28,6 @@ const navItems = [
     href: "/"
   },
   {
-    name: "Search",
-    icon: <HiSearch />,
-    href: "/search"
-  },
-  {
     name: "Wishlist",
     icon: <HiOutlineHeart />,
     href: "/wishlist"
@@ -42,10 +38,10 @@ const MobileNavbar = () => {
   const cart = useSelector((state) => state.cart)
   const router = useRouter()
   const [active, setActive] = useState(navItems[2])
-
+  console.log(active)
   return (
     <div className="flex sm:hidden z-50 w-full justify-center items-center fixed bottom-2">
-      <ul className=" rounded-2xl bg-gray-200 shadow-sm flex justify-center  items-center w-[90%] ">
+      <ul className=" rounded-3xl bg-white border-2 border-red-400/20 flex justify-center  items-center w-[90%] ">
         {cart.length > 0 && (
           <div
             className={`absolute left-[30%] bottom-8 bg-red-400 rounded-full h-5 w-5 flex justify-center items-center z-10 font-bold text-xs duration-300 ${
@@ -61,7 +57,7 @@ const MobileNavbar = () => {
           <li
             onClick={() => setActive(item)}
             key={item.name}
-            className="w-full px-2 flex justify-center items-center  h-12 "
+            className="w-full px-2 flex justify-center items-center h-12"
           >
             <Link href={item.href}>
               <div className="flex flex-col justify-center items-center">
@@ -85,6 +81,26 @@ const MobileNavbar = () => {
             </Link>
           </li>
         ))}
+        <li
+          onClick={() => setActive("Search")}
+          className="w-full px-2 flex justify-center items-center h-12"
+        >
+          <i
+            className={` flex justify-center items-center text-2xl p-3  absolute duration-300  ${
+              active == "Search" ? "" : ""
+            }`}
+          >
+            <Search />
+          </i>
+
+          {/* <span
+            className={`flex justify-center items-center absolute font-bold text-sm duration-300 transition-opacity ease-in-out ${
+              router.pathname == "search" ? "opacity-100" : "opacity-0"
+            }`}
+          >
+            Search
+          </span> */}
+        </li>
       </ul>
     </div>
   )
