ddd25644d97b106bd3193ae65d27de89855c5b92
Javad aghebati <j.aghebati93@gmail.com>
2022-05-17 23:55:38 +0430

    update styles

--- components/Products/Product/EmblaCarousel/EmblaThumb.js
+++ components/Products/Product/EmblaCarousel/EmblaThumb.js
@@ -2,18 +2,20 @@ import React from "react"
 import Image from "next/image"
 
 export const Thumb = ({ selected, onClick, imgSrc }) => (
-  <div className={`pl-3 relative min-w-[20%] ${selected ? "opacity-100" : ""}`}>
+  <div className={`relative min-w-[20%] bg-white  overflow-hidden px-1 `}>
     <button
       onClick={onClick}
       className="relative overflow-hidden h-20 touch-manipulation cursor-pointer w-full bg-transparent block"
       type="button"
     >
       <Image
-        className={`opacity-40 object-cover ${selected ? "opacity-100" : ""}`}
+        className={`opacity-40 object-cover overflow-hidden ${
+          selected ? "opacity-100" : ""
+        }`}
         src={imgSrc}
-        layout="fill"
-        w="0"
-        h="0"
+        layout="responsive"
+        width="0"
+        height="0"
         alt="A cool shoe."
       />
     </button>
