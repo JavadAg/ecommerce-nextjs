efde43a7013067af4019a9742091398e23ace1c8
Javad aghebati <j.aghebati93@gmail.com>
2022-05-23 23:57:20 +0430

    feat: create pages/dashboard/index.js

---
+++ pages/dashboard/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const index = () => {
+  return (
+    <div className="flex justify-center items-center text-center">
+      <div className="flex justify-center items-center text-center mt-16">
+        xxx
+      </div>
+    </div>
+  )
+}
+
+export default index
