0802848f8a2c58048ee50a28965541267d380734
Javad aghebati <j.aghebati93@gmail.com>
2022-05-23 23:57:35 +0430

    update cart.slice.js

--- redux/cart.slice.js
+++ redux/cart.slice.js
@@ -1,4 +1,4 @@
-import { createSlice, current } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit"
 
 const cartSlice = createSlice({
   name: "cart",
@@ -52,7 +52,7 @@ const cartSlice = createSlice({
   }
 })
 
-export default cartSlice
+export default cartSlice.reducer
 
 export const { addToCart, incrementQty, decrementQty, removeFromCart } =
   cartSlice.actions
