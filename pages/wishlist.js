589a2e9f66051fd3ce47f3bc9679fe2891b8bc02
Javad aghebati <j.aghebati93@gmail.com>
2022-05-23 23:56:18 +0430

    feat: create wishlist.js

---
+++ pages/wishlist.js
@@ -0,0 +1,12 @@
+import React from "react"
+import WishList from "../components/WishList/WishList"
+
+const wishlist = () => {
+  return (
+    <div>
+      <WishList />
+    </div>
+  )
+}
+
+export default wishlist
