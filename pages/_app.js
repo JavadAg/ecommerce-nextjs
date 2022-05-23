1536f421dbb039c0b06d8cfdeb5b3b4843bd7df2
Javad aghebati <j.aghebati93@gmail.com>
2022-05-23 23:56:11 +0430

    update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,27 @@
 import axios from "axios"
 import "../styles/globals.css"
-import store from "../redux/store"
-import { Provider } from "react-redux"
-import { persistStore } from "redux-persist"
-import { PersistGate } from "redux-persist/integration/react"
+import "@fontsource/quicksand"
 import Header from "../components/Header/Header"
 import Footer from "../components/Footer/Footer"
-import "@fontsource/quicksand"
+import store from "../redux/store"
+import { PersistGate } from "redux-persist/integration/react"
+import { SessionProvider } from "next-auth/react"
+import { Provider } from "react-redux"
+import { persistStore } from "redux-persist"
 
 let persistor = persistStore(store)
 
 function MyApp({ Component, pageProps, appProps }) {
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Header />
-        <div className="bg-gray-50">
-          <Component {...pageProps} appProps={appProps} />
-        </div>
-        <Footer />
+      <PersistGate persistor={persistor} loading={null}>
+        <SessionProvider>
+          <Header />
+          <div className="bg-gray-50">
+            <Component {...pageProps} appProps={appProps} />
+          </div>
+          <Footer />
+        </SessionProvider>
       </PersistGate>
     </Provider>
   )
