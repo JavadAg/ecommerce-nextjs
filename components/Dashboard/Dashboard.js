d11c1ff71e1e024a36e802ad25d712fc3db346c4
Javad aghebati <j.aghebati93@gmail.com>
2022-05-25 02:36:53 +0430

    feat: create Dashboard.js

---
+++ components/Dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Dashboard = () => {
+  return (
+    <div className="flex justify-center items-center text-center">
+      <div className="flex justify-center items-center text-center mt-16">
+        dashboard
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
