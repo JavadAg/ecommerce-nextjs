88f1fd4f74a0f8b26c8570c18d0941e70e70cc12
Javad aghebati <j.aghebati93@gmail.com>
2022-05-26 02:40:33 +0430

    updated img domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ["img.stadiumgoods.com", "localhost"] },
+  images: { domains: ["localhost"] },
   reactStrictMode: true
 }
 
