fd23a727e9fe1a976f233ad2da8b72b31562970c
Javad aghebati <j.aghebati93@gmail.com>
2022-06-08 04:21:06 +0430

    update Products.js

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -8,7 +8,7 @@ const Products = ({ data }) => {
         <div className="py-4 px-2 md:px-10 w-full xl:px-36">
           <div className="grid grid-cols-2 grid-rows-2 gap-y-4 gap-x-4 md:grid-cols-3 xl:grid-cols-4">
             {data?.map((item) => (
-              <Product item={item} />
+              <Product key={item.id} item={item} />
             ))}
           </div>
         </div>
